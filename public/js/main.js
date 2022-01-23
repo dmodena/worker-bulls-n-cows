c0b7cc36ea743cfc9e9622cdd0e36fcd30a96d8d
Douglas Modena <d.modena@live.com>
2022-01-23 12:51:38 -0300

    Number pad and backspace working

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
 (function() {
     const worker = new Worker('js/worker.js');
+    const guessInputView = document.getElementsByClassName('guess-input-view');
     const guessInput = document.getElementById('guessInput');
     const startGameBtn = document.getElementById('startGameBtn');
+    const numBtn0 = document.getElementById('numBtn0');
+    const numBtn1 = document.getElementById('numBtn1');
+    const numBtn2 = document.getElementById('numBtn2');
+    const numBtn3 = document.getElementById('numBtn3');
+    const numBtn4 = document.getElementById('numBtn4');
+    const numBtn5 = document.getElementById('numBtn5');
+    const numBtn6 = document.getElementById('numBtn6');
+    const numBtn7 = document.getElementById('numBtn7');
+    const numBtn8 = document.getElementById('numBtn8');
+    const numBtn9 = document.getElementById('numBtn9');
+    const backspaceBtn = document.getElementById('backspaceBtn');
     const guessBtn = document.getElementById('guessBtn');
     const giveUpBtn = document.getElementById('giveUpBtn');
     const statusInput = document.getElementById('statusInput');
-    const resultTextArea = document.getElementById('resultTextArea');
+    const historyList = document.getElementById('historyList');
 
     let guesses = 0;
+    let lastGuess = [];
 
     const getDigits = function(number) {
         if (isNaN(number))
@@ -16,12 +29,49 @@
         return number.toString().split('').map(s => parseInt(s));
     }
 
+    const createHistoryItem = function(guessResult) {
+        let guessAttempt = guesses < 10
+            ? "0" + guesses.toString()
+            : guesses.toString();
+
+        return `<li class="list-group-item">${guessAttempt}. ${lastGuess} - ${guessResult}</li>`;
+    }
+
+    const updateInputView = function() {
+        let guessInputStrList = guessInput.value.toString().split('');
+
+        for (i = 0; i < 4; i++) {
+            guessInputView[i].value = guessInputStrList[i] ?? '';
+        }
+    }
+
+    const typeNumber = function(number) {
+        let guessInputLength = guessInput.value?.toString().length ?? 0;
+
+        if (guessInputLength > 3)
+            return;
+
+        guessInput.value += number.toString();
+        updateInputView();
+    }
+
+    const typeBackspace = function() {
+        let guessInputLength = guessInput.value?.toString().length ?? 0;
+
+        if (guessInputLength == 0)
+            return;
+
+        guessInput.value = guessInput.value.slice(0, -1);
+        updateInputView();
+    }
+
     const startGame = function() {
         worker.postMessage({ fn: 'startGame' });
     }
 
     const guessNumber = function() {
         const guessedNumber = getDigits(guessInput.value);
+        lastGuess = guessedNumber;
 
         worker.postMessage({ fn: 'guess', val: guessedNumber });
     }
@@ -39,7 +89,7 @@
                 statusInput.value = message.data.val;
                 break;
             case 'guess':
-                resultTextArea.value += `${++guesses}. ${message.data.val}\n`
+                historyList.value = createHistoryItem(message.data.val) + historyList.value;
             default:
                 break;
         }
@@ -47,6 +97,17 @@
 
     worker.onmessage = receiveFromWorker;
     startGameBtn.addEventListener('click', startGame);
+    numBtn0.addEventListener('click', () => typeNumber(0));
+    numBtn1.addEventListener('click', () => typeNumber(1));
+    numBtn2.addEventListener('click', () => typeNumber(2));
+    numBtn3.addEventListener('click', () => typeNumber(3));
+    numBtn4.addEventListener('click', () => typeNumber(4));
+    numBtn5.addEventListener('click', () => typeNumber(5));
+    numBtn6.addEventListener('click', () => typeNumber(6));
+    numBtn7.addEventListener('click', () => typeNumber(7));
+    numBtn8.addEventListener('click', () => typeNumber(8));
+    numBtn9.addEventListener('click', () => typeNumber(9));
+    backspaceBtn.addEventListener('click', typeBackspace);
     guessBtn.addEventListener('click', guessNumber);
     giveUpBtn.addEventListener('click', giveUp);
 })();
